6a469ac145c54b8af4f7b7b814e16f10c23e1102
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-05-31 14:49:29 +0200

    INIT

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/*var emailUtente = prompt();
+
+console.log(emailUtente);
+
+var listaAdmin = ['marco@gmail.com', 'edoardo.ragazzi@gmail.com', 'elena@gmail.com'];
+
+
+
+for (i = 0; i < listaAdmin.length; i++) {
+
+    if (emailUtente == listaAdmin[i]) {
+        console.log('Email corretta');
+    }
+    console.log('Email sbagliata');
+}
+*/
+//------LISTA COGNOME--------
+var cognomeUtente = prompt();
+
+var listaCognomi = ['Bianchi', 'Zeri', 'Rossi', 'Verdi', 'Gialli'];
+
+console.log(listaCognomi);
+
+listaCognomi.push(cognomeUtente);
+
+listaCognomi.sort();
+
+console.log(listaCognomi);
+
+console.log(listaCognomi[i + 1]);
